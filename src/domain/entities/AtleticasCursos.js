3955cac0c6bc6bfecf2a9a550afb17339b8233d1
Lucas Carvalho <caldoxgamer@gmail.com>
2021-09-15 09:52:02 -0300

    criando entidades

---
+++ src/domain/entities/AtleticasCursos.js
@@ -0,0 +1,26 @@
+const {
+  BadRequestException,
+} = require("../../presentation/errors/BadRequestException");
+
+class AtleticasCursosEntity {
+  id_curso = null;
+  id_atletica = null;
+
+  constructor(data) {
+    Object.assign(this, data);
+    Object.freeze(this);
+    this.#validate();
+  }
+
+  #validate() {
+    if (!this.id_curso) {
+      throw new BadRequestException("Property 'id_curso' is required.");
+    }
+
+    if (!this.id_atletica) {
+      throw new BadRequestException("Property 'id_atletica' is required.");
+    }
+  }
+}
+
+exports.AtleticasCursosEntity = AtleticasCursosEntity;
