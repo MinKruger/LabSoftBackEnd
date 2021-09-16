ba9c559ef6007924a09b9d0af1b3e9b241df5823
Lucas Carvalho <caldoxgamer@gmail.com>
2021-09-16 16:13:03 -0300

    criando documentaçao da api pelo swagget

--- src/app/app.js
+++ src/app/app.js
@@ -2,6 +2,9 @@ require("../infra/database/index");
 require("express-async-errors");
 
 const express = require("express");
+const swaggerUI = require("swagger-ui-express");
+
+const swaggerDocs = require("../shared/docs/swagger.json");
 const {
   handleErrorsMiddleware,
 } = require("../presentation/middlewares/handleErrors");
@@ -10,6 +13,7 @@ const { AtleticaRouter } = require("./routes/atleticaRoutes");
 const app = express();
 
 app.use(express.json());
+app.use("/docs", swaggerUI.serve, swaggerUI.setup(swaggerDocs));
 
 app.use("/atletica", AtleticaRouter);
 
