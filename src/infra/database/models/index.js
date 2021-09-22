307d05df08e3d1418412df0ae88fdf39f54dbac0
andreefl <andre_efl@hotmail.com>
2021-09-22 20:40:37 -0300

    criando model Usuario

--- src/infra/database/models/index.js
+++ src/infra/database/models/index.js
@@ -1,7 +1,9 @@
 const { AtleticaModel } = require("./Atletica");
 const { CursoModel } = require("./Curso");
+const { UsuarioModel } = require("./Usuario");
 
 exports.Models = {
   AtleticaModel,
   CursoModel,
+  UsuarioModel,
 };
