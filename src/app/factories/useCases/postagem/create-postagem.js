0ab20e4e0f2bcc95c0c82c07c04d4fc2fe610cbf
Lucas Carvalho Honorio <43136700+LuskasDMC@users.noreply.github.com>
2021-10-05 19:14:30 -0300

    Lch/modulo noticias (#13)
    * adicionando coluna instagram na tabela de atleticas
    
    * criando migration para ajustar tabela de postagens
    
    * criando migration para dropar as tabelas cargos, campeonatos, atletica_campeonato e comentarios
    
    * criando migration para criaçao das tabelas, dce, participantes, parceiros e liga_das_atleticas
    
    * criando migration para ajustar as tabelas atleticas, alunos, modalidades, fases e jogos
    
    * adicionando transaction nas migrations para resolver inconsistencia em casos de falhas em migrations que tem muitas alteraçoes
    
    * criando fluxo para criaçao de uma postagem
    
    * criando fluxo para buscar todas as postagens
    
    * adding code smell
    
    * criando fluxo para buscar uma postagem pelo id
    
    * criando fluxo de update postagem
    
    * criando fluxo de delete postagem
    
    * implementando paginaçao na busca de postagens
    
    * resolvendo alguns code smells
    
    * implementando satinizaçao para tratar possiveis tentativas de XSS
    
    * mudando mensagem
    
    Co-authored-by: Lucas Carvalho Honorio <lucas.carvalho@pixter.com.br>

--- src/app/factories/useCases/postagem/create-postagem.js
+++ src/app/factories/useCases/postagem/create-postagem.js
@@ -2,13 +2,17 @@ const {
   PostagemRepository,
 } = require("../../../../data/repositories/PostagemRepository");
 const {
+  UsuarioRepository,
+} = require("../../../../data/repositories/UsuarioRepository");
+const {
   CreatePostagemUseCase,
 } = require("../../../../data/useCases/postagem/create-postagem");
 
 const createPostagemFactory = function () {
   const postagemRepository = new PostagemRepository();
+  const usuarioRepository = new UsuarioRepository();
 
-  return new CreatePostagemUseCase(postagemRepository);
+  return new CreatePostagemUseCase(postagemRepository, usuarioRepository);
 };
 
 exports.createPostagemFactory = createPostagemFactory;
