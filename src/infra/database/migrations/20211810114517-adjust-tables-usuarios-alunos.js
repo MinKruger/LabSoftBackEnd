695b4ed372224bdec0b9acb08ae9fe3a77ad7756
Lucas Carvalho Honorio <43136700+LuskasDMC@users.noreply.github.com>
2021-10-18 22:42:38 -0300

    Aefl/cadastro usuario (#24)
    * não retornar senha nas queries de usuario
    
    * register usuario
    
    * criptografia de senha
    
    * tratamento de erros
    
    * Ajustando banco (atleticas, jogos, usuarios)
    
    * corrigindo seeds de atleticas e usuarios
    
    * adicionando auth à autenticação
    
    * Migration ajustando usuarios e alunos
    
    * Ajustando model Usuario
    
    * Ajustando campos de usuário
    
    * Adicionando campo id_atletica em usuarios
    
    * Ajustando funções de usuario
    
    * Retornar JWT no registro
    
    * Atualizando documentação
    
    * ajustes
    
    Co-authored-by: andreefl <andre_efl@hotmail.com>

--- src/infra/database/migrations/20211810114517-adjust-tables-usuarios-alunos.js
+++ src/infra/database/migrations/20211810114517-adjust-tables-usuarios-alunos.js
@@ -2,104 +2,153 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.addColumn("usuarios", "email", {
-      type: Sequelize.STRING(100),
-      allowNull: false,
-      unique: true,
-    });
-    await queryInterface.addColumn("usuarios", "nome", {
-      type: Sequelize.STRING(200),
-      allowNull: false,
-    });
-    await queryInterface.addColumn("usuarios", "instagram", {
-      type: Sequelize.STRING(100),
-      allowNull: true,
-    });
-    await queryInterface.addColumn("usuarios", "permissao", {
-      type: Sequelize.ENUM('aluno', 'atletica', 'dce1', 'dce2', 'dce3'),
-      allowNull: false,
-      defaultValue: 'aluno',
-    });
+    await queryInterface.sequelize.transaction(async (transaction) => {
+      await queryInterface.addColumn(
+        "usuarios",
+        "email",
+        {
+          type: Sequelize.STRING(100),
+          allowNull: true,
+          unique: true,
+        },
+        { transaction }
+      );
+      await queryInterface.addColumn(
+        "usuarios",
+        "nome",
+        {
+          type: Sequelize.STRING(200),
+          allowNull: false,
+        },
+        { transaction }
+      );
+      await queryInterface.addColumn(
+        "usuarios",
+        "instagram",
+        {
+          type: Sequelize.STRING(100),
+          allowNull: true,
+        },
+        { transaction }
+      );
+      await queryInterface.addColumn(
+        "usuarios",
+        "permissao",
+        {
+          type: Sequelize.ENUM("aluno", "atletica", "dce1", "dce2", "dce3"),
+          allowNull: false,
+          defaultValue: "aluno",
+        },
+        { transaction }
+      );
 
-    await queryInterface.removeColumn("usuarios", "tipo");
-    await queryInterface.removeColumn("usuarios", "login");
+      await queryInterface.removeColumn("usuarios", "tipo", { transaction });
+      await queryInterface.removeColumn("usuarios", "login", { transaction });
 
-    await queryInterface.addColumn("alunos", "matricula", {
-      type: Sequelize.STRING(9),
-      allowNull: false,
+      await queryInterface.addColumn(
+        "alunos",
+        "matricula",
+        {
+          type: Sequelize.STRING(9),
+          allowNull: false,
+        },
+        { transaction }
+      );
+
+      await queryInterface.removeColumn("alunos", "nome", { transaction });
+
+      await queryInterface.dropTable("dce", { transaction });
     });
-    
-    await queryInterface.removeColumn("alunos", "nome");
-
-    await queryInterface.dropTable("dce");
   },
 
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.removeColumn("usuarios", "email");
-    await queryInterface.removeColumn("usuarios", "nome");
-    await queryInterface.removeColumn("usuarios", "instagram");
-    await queryInterface.removeColumn("usuarios", "permissao");
+    await queryInterface.sequelize.transaction(async (transaction) => {
+      await queryInterface.removeColumn("usuarios", "email", { transaction });
+      await queryInterface.removeColumn("usuarios", "nome", { transaction });
+      await queryInterface.removeColumn("usuarios", "instagram", {
+        transaction,
+      });
+      await queryInterface.removeColumn("usuarios", "permissao", {
+        transaction,
+      });
 
-    await queryInterface.addColumn("usuarios", "tipo", {
-      type: Sequelize.ENUM('aluno', 'atletica', 'dce'),
-      allowNull: false,
-      defaultValue: 'aluno',
-    });
-    await queryInterface.addColumn("usuarios", "login", {
-      type: Sequelize.STRING(100),
-      allowNull: false,
-      unique: true,
-    });
-
-    await queryInterface.removeColumn("alunos", "matricula");
-
-    await queryInterface.addColumn("alunos", "nome", {
-      type: Sequelize.STRING(250),
-      allowNull: false,
-    });
-
-    await queryInterface.createTable(
-      "dce",
-      {
-        id: {
-          type: Sequelize.UUID,
-          defaultValue: Sequelize.UUIDV4,
+      await queryInterface.addColumn(
+        "usuarios",
+        "tipo",
+        {
+          type: Sequelize.ENUM("aluno", "atletica", "dce"),
           allowNull: false,
-          primaryKey: true,
+          defaultValue: "aluno",
         },
-        nome: {
+        { transaction }
+      );
+      await queryInterface.addColumn(
+        "usuarios",
+        "login",
+        {
+          type: Sequelize.STRING(100),
+          allowNull: false,
+          unique: true,
+        },
+        { transaction }
+      );
+
+      await queryInterface.removeColumn("alunos", "matricula", { transaction });
+
+      await queryInterface.addColumn(
+        "alunos",
+        "nome",
+        {
           type: Sequelize.STRING(250),
           allowNull: false,
         },
-        cargo: {
-          type: Sequelize.INTEGER,
-          allowNull: false,
-        },
-        instagram: {
-          type: Sequelize.STRING(250),
-          allowNull: true,
-        },
-        id_usuario: {
-          type: Sequelize.UUID,
-          allowNull: false,
-          references: {
-            model: {
-              tableName: "usuarios",
+        { transaction }
+      );
+
+      await queryInterface.createTable(
+        "dce",
+        {
+          id: {
+            type: Sequelize.UUID,
+            defaultValue: Sequelize.UUIDV4,
+            allowNull: false,
+            primaryKey: true,
+          },
+          nome: {
+            type: Sequelize.STRING(250),
+            allowNull: false,
+          },
+          cargo: {
+            type: Sequelize.INTEGER,
+            allowNull: false,
+          },
+          instagram: {
+            type: Sequelize.STRING(250),
+            allowNull: true,
+          },
+          id_usuario: {
+            type: Sequelize.UUID,
+            allowNull: false,
+            references: {
+              model: {
+                tableName: "usuarios",
+              },
+              key: "id",
             },
-            key: "id",
+          },
+          created_at: {
+            type: Sequelize.DATE(3),
+            defaultValue: Sequelize.literal("CURRENT_TIMESTAMP(3)"),
+          },
+          updated_at: {
+            type: Sequelize.DATE(3),
+            defaultValue: Sequelize.literal(
+              "CURRENT_TIMESTAMP(3) ON UPDATE CURRENT_TIMESTAMP(3)"
+            ),
           },
         },
-        created_at: {
-          type: Sequelize.DATE(3),
-          defaultValue: Sequelize.literal("CURRENT_TIMESTAMP(3)"),
-        },
-        updated_at: {
-          type: Sequelize.DATE(3),
-          defaultValue: Sequelize.literal(
-            "CURRENT_TIMESTAMP(3) ON UPDATE CURRENT_TIMESTAMP(3)"
-          ),
-        },
-      }
-    );
+        { transaction }
+      );
+    });
   },
 };
